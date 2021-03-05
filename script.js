0bede4710f96b08ec5620cbfbfe379eb8807e8a2
Suejin Kim <kim.suejin.20@gmail.com>
2021-03-04 22:25:49 -0500

    added comments for next steps

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
-//var timeTracker = ''moment().format("dddd, MMMM Do YYYY, h:mm:ss a");''
-//$("#time-tracker").text(timeTracker);
+var modalEl = $("#project-modal");
+var addProjectBtn = $("#addProjectBtn");
 
+
+//Display Current Time in Header
 var update = function() {
-    document.getElementById("time-tracker")
+    $("#time-tracker")
     .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
 }
 setInterval(update, 1000);
 
 
-$( function() {
-    $( "#dialog" ).dialog();
-  } );
+
+function init() {
+
+  // Opens modal/dialog box
+  $(function () {
+    $("#dialog").dialog( {
+      modal: true,
+      autoOpen: false,
+      title: "Add Project Information:",
+    })
+    $("#addProjectBtn").click(function () {
+      $('#dialog').dialog('open');
+    })
+  })
+
+//create elements to match the fields from the input form
+var projectEl = $("#project-name")
+var typeEl = $("#project-type")
+
+//receive the value from input and store in new variables
+
+
+//append that information to the table
+
+
+//figure out the x button
+
+
+
+
+
+
+
+
+};
+
+
+
+// Code for date picker in form
 var dueDateInputEl = $('#due-date-input');
 
 // later in your script
 dueDateInputEl.datepicker({ minDate: 1 });
+
+init();
